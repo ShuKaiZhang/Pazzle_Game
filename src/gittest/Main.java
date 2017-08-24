7c93966b6ca99f8b455eb9467d9248fb7dcca5a6
Cindy <u5540796@anu.edu.au>
2017-08-24 12:20:02 +1000

    24.Aug

--- src/gittest/Main.java
+++ src/gittest/Main.java
@@ -2,6 +2,10 @@ package gittest;
 
 public class Main {
     public static void main(String[] args) {
+        String[] bob = {"bob", "alice","Cindy"};
+        System.out.println(bob[1]);
+        System.out.println(bob[0]);
+        System.out.println(bob[2]);
 
     }
 }
