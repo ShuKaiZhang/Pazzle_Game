37c3f149e1062f22bd6a2c7c35e2714a283914c1
Vithuran Jeyasingham <u5845250@anu.edu.au>
2017-10-05 11:04:48 +1100

    Update U5845250 originality

---
+++ src/comp1110/ass2/BasicStarts.java
@@ -0,0 +1,9 @@
+package comp1110.ass2;
+
+public class BasicStarts {
+    String starter [] = {"AHGBGSCDNDAiEFBGCgHFl", ""};
+    String junior [] = {""};
+    String expert [] = {""};
+    String master [] = {""};
+    String hard [] = {""};
+}
